e808492d5969c3041a725fb6b1b336c0b393169c
Mina Shaigan <mina.shaigan@gmail.com>
2019-08-10 03:52:06 +0430

    add files for race with nultiple things

---
+++ multiple-race/setup.py
@@ -0,0 +1,11 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("race.py")]
+
+cx_Freeze.setup(
+    name = "Rainy Formula",
+    options = {"build_exe" : {  "packages" : ["numpy", "pygame"], 
+				"include_files": ["racecar.png", "caricon.png", "crash.wav", "jazz.wav"]
+}},
+    executables = executables
+)
